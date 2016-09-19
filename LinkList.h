8383df92cc8268c1444d6188adb0074946373937
yewei wang <951124394@qq.com>
2016-09-19 10:12:15 +0800

    Create LinkList.h

---
+++ LinkList.h
@@ -0,0 +1,29 @@
+#ifndef __LINKLIST_H_
+
+enum Comparison
+{
+	equal, greater, smaller
+};
+
+#define ElemType int
+
+enum STATUS
+{
+	INFEASIBLE = -2, ERROR = -1, FALSE = 0, TRUE = -1, OK = 2
+};
+
+typedef struct List
+{
+	ElemType data;
+	struct List *next;
+}List;
+
+List* CreateListT(int n);
+List* CreateListH(int n);
+int GetListLength(List *L);
+ElemType GetElem(List *list, int x);
+void ListInsert(List *list, int x, ElemType e);
+bool LocateElem(List *list, int e, Comparison relation);
+bool ListDelete(List *list, ElemType e);
+
+#endif /*__LINKLIST_H__
